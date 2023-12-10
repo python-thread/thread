30ee6af2ee9cf539d06e2396c800ec4a909ed9fc
AlexNg <taboez4life@gmail.com>
2023-12-10 13:09:55 +0800

    + Change import path

--- src/thread/cli/__init__.py
+++ src/thread/cli/__init__.py
@@ -3,7 +3,7 @@ Import and config CLI commands
 """
 
 __version__ = '0.1.2'
-from ..config import logging, ColorLogger
+from ..utils.logging_config import logging, ColorLogger
 logging.setLoggerClass(ColorLogger)
 
 
