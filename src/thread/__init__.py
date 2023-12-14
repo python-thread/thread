fa81450a890be39ba64838605a4bb7be31fcbe9c
AlexNg <taboez4life@gmail.com>
2023-12-14 22:58:53 +0800

    + Added docstring & exported types

--- src/thread/__init__.py
+++ src/thread/__init__.py
@@ -1,10 +1,34 @@
+"""
+## Thread Library
+Documentation at https://thread.ngjx.org
+
+
+---
+
+Released under the GPG-3 License
+
+Copyright (c) thread.ngjx.org, All rights reserved
+"""
+
+"""
+This file contains the exports to
+```py
+import thread
+```
+"""
+
+
+# Export Core
 from .thread import (
   Thread,
-  ParallelProcessing,
+  ParallelProcessing
 )
 
+
 from . import (
+  _types as types,
   exceptions
 )
 
+# Configuration
 from .utils import Settings
