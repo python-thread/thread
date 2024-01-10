8bddf598014be31fa50a53e3b6fb6e394dd006ea
AlexNg <taboez4life@gmail.com>
2024-01-10 20:16:37 +0800

    + Adding processor test cases

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,5 +1,5 @@
 import time
-from src.thread import threaded
+from src.thread import threaded, processor
 
 
 # >>>>>>>>>> Dummy Functions <<<<<<<<<< #
@@ -43,3 +43,48 @@ def test_threadedArgJoin():
   
   x = _run(8, 9)
   assert x.get_return_value() == (1, 2, 3, 8, 9)
+
+
+
+
+def test_processorCreationNoParam():
+  @processor
+  def _run(args):
+    return _dummy_target_raiseToPower(*args)
+  
+  x = _run([[2, 2]])
+  assert x.get_return_values() == [4]
+
+def test_processorCreationEmptyParam():
+  @processor()
+  def _run(args):
+    return _dummy_target_raiseToPower(*args)
+  
+  x = _run([[2, 2]])
+  assert x.get_return_values() == [4]
+
+def test_processorCreationWithParam():
+  @processor(daemon = True)
+  def _run(args):
+    return _dummy_target_raiseToPower(*args)
+  
+  x = _run([[2, 2]])
+  assert len(x._threads) == 1
+  assert x._threads[0].thread.daemon
+  assert x.get_return_values() == [4]
+
+def test_processorArgJoin():
+  @processor(daemon = True, args = (1, 2, 3))
+  def _run(data, *args):
+    return [*args, *data]
+  
+  x = _run([[8, 9]])
+  assert x.get_return_values() == [[1, 2, 3, 8, 9]]
+
+def test_processorMultiArgJoin():
+  @processor(daemon = True, args = (1, 2, 3))
+  def _run(data, *args):
+    return [*args, *data]
+  
+  x = _run([[8, 9], [10, 11]])
+  assert x.get_return_values() == [[1, 2, 3, 8, 9], [1, 2, 3, 10, 11]]
