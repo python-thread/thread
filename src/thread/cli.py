f870f0e394d2e692be9c875750492d93af020873
AlexNg <taboez4life@gmail.com>
2024-02-04 23:24:57 +0800

    CLI migration

---
+++ src/thread/cli.py
@@ -0,0 +1,9 @@
+
+try:
+  import importlib
+  thread_cli = importlib.import_module('thread-cli')
+  app = thread_cli.app
+except ModuleNotFoundError:
+  def app(prog_name = 'thread'):
+    print('thread-cli not found, please install it with `pip install thread-cli`')
+    exit(1)
