baf4f910386ca982ab85272212843d8fd30c22ae
AlexNg <taboez4life@gmail.com>
2024-03-11 23:57:33 +0800

    style: Code formatting

--- src/thread/utils/config.py
+++ src/thread/utils/config.py
@@ -4,16 +4,16 @@ class Settings:
     `Non Instantiable`
     """
 
-  VERBOSITY: bool = True
-  GRACEFUL_EXIT_ENABLED: bool = True
+    VERBOSITY: bool = True
+    GRACEFUL_EXIT_ENABLED: bool = True
 
     def __init__(self):
         raise NotImplementedError('This class is not instantiable')
 
-  @staticmethod
-  def set_graceful_exit(enabled: bool = True):
-    Settings.GRACEFUL_EXIT_ENABLED = enabled
+    @staticmethod
+    def set_graceful_exit(enabled: bool = True):
+        Settings.GRACEFUL_EXIT_ENABLED = enabled
 
-  @staticmethod
-  def set_verbosity(verbosity: bool = True):
-    Settings.VERBOSITY = verbosity
+    @staticmethod
+    def set_verbosity(verbosity: bool = True):
+        Settings.VERBOSITY = verbosity
