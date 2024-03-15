1d453e0255497908c9e8d48f0edb3dc6f4054c2b
AlexNg <taboez4life@gmail.com>
2024-03-15 21:36:49 +0800

    feat: Include protocols
    Enables partial support with overloading
    __getitem__ for accessing with [int] syntax (Example: [1, 2][0])
    __len__ for compatibility with the len() function
    
    Ref: #67

--- src/thread/_types.py
+++ src/thread/_types.py
@@ -4,8 +4,14 @@
 Documentation: https://thread.ngjx.org/docs/v1.0.0
 """
 
-from typing import Any, Literal, Callable, Union
-from typing_extensions import ParamSpec, TypeVar, Concatenate
+from typing import Any, Literal, Callable, Union, Sized
+from typing_extensions import (
+    ParamSpec,
+    TypeVar,
+    Concatenate,
+    Protocol,
+    runtime_checkable,
+)
 
 
 # Descriptive Types
@@ -35,3 +41,19 @@ HookFunction = Callable[[_Target_T], Union[Any, None]]
 
 _Dataset_T = TypeVar('_Dataset_T', covariant=True)
 DatasetFunction = Callable[Concatenate[_Dataset_T, _Target_P], _Target_T]
+
+
+# Protocols
+@runtime_checkable
+class SupportsLength(Sized, Protocol):
+    pass
+
+
+@runtime_checkable
+class SupportsGetItem(Protocol[_Dataset_T]):
+    def __getitem__(self, i: int) -> _Dataset_T: ...
+
+
+@runtime_checkable
+class SupportsLengthGetItem(SupportsGetItem[_Dataset_T], SupportsLength, Protocol):
+    pass
